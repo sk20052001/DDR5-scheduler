51bccec3881216a46154b87e8a325e4c79d67886
sk20052001 <sanjeev@pdx.edu>
2025-03-06 17:04:52 -0800

    added open page

--- ddr5_command.c
+++ ddr5_command.c
@@ -20,14 +20,26 @@ void enqueue() {
 	queue[rear].core = core;
 	queue[rear].opCode = opCode;
 	queue[rear].row = (address >> 18) & ((1 << 16) - 1);
-	// queue[rear].upperCol = (address >> 12) & (0x3F);
-	// queue[rear].lowerCol = (address >> 2) & (0xF);
 	queue[rear].col = (((address >> 12) & (0x3F)) << 4) | ((address >> 2) & (0xF));
 	queue[rear].bnk = (address >> 10) & (0x3);
 	queue[rear].bnkGp = (address & ((0x7) << 7)) >> 7;
-	queue[rear].channel = (address & (1 << 6)) >> 6;
+	queue[rear].chanel = (address & (1 << 6)) >> 6;
 	queue[rear].bank= (queue[rear].bnkGp * 4) + queue[rear].bnk;
 }
+
+void deq()
+{
+	if(front == rear)
+	{		
+		rear = front = - 1;
+	}
+
+	else 
+	{
+		front = (front + 1) % numberOfBanks;
+	}	
+}
+
 
 void delay(int clockCycles) {
 	for (int i = 0; i < clockCycles; i++) {
@@ -49,13 +61,80 @@ void delay(int clockCycles) {
 	}
 }
 
+int isFull() {
+	if ((front == rear + 1) || (front == 0 && rear == numberOfBanks - 1)) return 1;
+	return 0;
+}
+  
+int empty() {
+	return front==-1;
+}
+
 void dimm(struct queue queue) {
-	if (bank[queue.bank].status == 0) {
-		fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.channel, 6, "ACT0", 3, queue.bnkGp, 3, queue.bnk, 3, queue.row);
+	if (bank[queue.bnk].status==0) {
+		fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "ACT0", 3, queue.bnkGp, 3, queue.bank, 3, queue.row);		
 		delay(2);
-		fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.channel, 6, "ACT1", 3, queue.bnkGp, 3, queue.bnk, 3, queue.row);
-		bank;
-	}
+		fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "ACT1", 3, queue.bnkGp, 3, queue.bank, 3, queue.row);
+		bank[queue.bnk].status=1;
+		bank[queue.bnk].row=queue.row;
+		delay(2*trcd);
+		if(queue.opCode!=1) {
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "RD0", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2);
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "RD1", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2*(tcl+tburst));
+			deq();
+			incrementFlag=1;
+			return;
+		} else {
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "WR0", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2);
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "WR1", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2*(cwl+tburst+twr));
+			deq();
+			incrementFlag=1;
+			return;
+		}
+	} else if (bank[queue.bnk].status == 1 && bank[queue.bnk].row != queue.row) {
+		fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d\n", 10, cpu, 3, queue.chanel, 6, "PRE", 3, queue.bnkGp, 3, queue.bank);
+		delay(2*trp);
+		fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "ACT0", 3, queue.bnkGp, 3, queue.bank, 3, queue.row);		
+		delay(2);
+		fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "ACT1", 3, queue.bnkGp, 3, queue.bank, 3, queue.row);
+		bank[queue.bnk].status=1;
+		bank[queue.bnk].row=queue.row;
+		delay(2*trcd);
+		if(queue.opCode!=1) {
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "RD0", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2);
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "RD1", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2*(tcl+tburst));
+		} else {
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "WR0", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2);
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "WR1", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2*(cwl+tburst+twr));
+		}
+		deq();
+		incrementFlag=1;
+		return;
+	} else {
+		if(queue.opCode!=1) {
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "RD0", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2);
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "RD1", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2*(tcl+tburst));
+			
+		} else {
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "WR0", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2);
+			fprintf(outputFile, "%-*lu%-*d%-*s%-*d%-*d%-*x\n", 10, cpu, 3, queue.chanel, 6, "WR1", 3, queue.bnkGp, 3, queue.bank, 3, queue.col);
+			delay(2*(cwl+tburst+twr));
+		}
+		deq();
+		incrementFlag=1;
+		return;
+	}	
 }
 
 int main(int argc, char *argv[3]) {
@@ -69,14 +148,14 @@ int main(int argc, char *argv[3]) {
 		inputFile = fopen("trace.txt", "r");
 	}
 
-	// if(argc>2)
-	// {
-	// 	fw=fopen(argv[2], "w");
-	// }
-	// else
-	// {
-	// 	fw=fopen("dram.txt","w");
-	// }
+	if(argc>2)
+	{
+		outputFile=fopen(argv[2], "w");
+	}
+	else
+	{
+		outputFile=fopen("dram.txt","w");
+	}
 
     for (int i = 0; i < numberOfBanks; i++) {
         bank[i].row = -1;
--- headers.h
+++ headers.h
@@ -1,10 +1,29 @@
 #define numberOfBanks 32
 #define queueSize 16
 
+#define tburst 8
+#define trcd 39
+#define tras 76
+#define trrdl 12
+#define trrds 8
+#define trtp 18
+#define trp 39
+#define tcl 40
+#define cwl 38
+#define twr 30
+#define tccdl 12
+#define tccds 8
+#define tccdlw 48
+#define tccdsw 8
+#define tccdlrtw 16
+#define tccdsrtw 16
+#define tccdlwtr 70
+#define tccdswtr 52
+
 struct bank {
 	int status, row;
 };
 
 struct queue {
-	int time, core, opCode, row, col, channel, bnk, bnkGp, bank;
+	int time, core, opCode, row, col, chanel, bnk, bnkGp, bank;
 };
